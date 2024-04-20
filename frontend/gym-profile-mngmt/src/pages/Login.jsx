b715d37167b99c32caa6095718a1c6492d4e4024
notntpete <berhelentize@gmail.com>
2024-04-21 00:51:41 +0900

    Test Commit

--- frontend/gym-profile-mngmt/src/pages/Login.jsx
+++ frontend/gym-profile-mngmt/src/pages/Login.jsx
@@ -4,33 +4,33 @@ import React, { useState, useEffect } from "react";
 
 function Login() {
   return (
-    <div class="bg-gradient-to-r from-gray-900 to-gray-600">
-      <div class="h-screen w-screen flex justify-center flex-col p-[50px]">
-        <div class="h-[580px] w-[680px] rounded-t border-none border-[2px] bg-gray-300 ">
-          <div class="text-left p-10 text-9xl">
+    <div className="bg-gradient-to-r from-gray-900 to-gray-600">
+      <div className="h-screen w-screen flex justify-center flex-col p-[50px]">
+        <div className="h-[580px] w-[680px] rounded-t border-none border-[2px] bg-gray-300 ">
+          <div className="text-left p-10 text-9xl">
             LOGIN
           </div> 
-          <div class="h-[200px] flex items-center flex-col space-y-10 justify-center">
-            <div class="">
-              <input type="password" class="h-[35px] w-[400px] rounded-lg pl-[10px] text-lg" placeholder="username"></input>
+          <div className="h-[200px] flex items-center flex-col space-y-10 justify-center">
+            <div className="">
+              <input type="password" className="h-[35px] w-[400px] rounded-lg pl-[10px] text-lg" placeholder="username"></input>
             </div>
-            <div class="">
-              <input type="text" class="h-[35px] w-[400px] rounded-lg pl-[10px] text-lg" placeholder="password"></input>
+            <div className="">
+              <input type="text" className="h-[35px] w-[400px] rounded-lg pl-[10px] text-lg" placeholder="password"></input>
             </div>
           </div>
-          <div class="w-[540px] flex flex-row-reverse items-center">
-            <button class="bg-stone-800 hover:bg-black text-white font-bold py-2 px-4 rounded">
+          <div className="w-[540px] flex flex-row-reverse items-center">
+            <button className="bg-stone-800 hover:bg-black text-white font-bold py-2 px-4 rounded">
               login
             </button>
           </div>
         </div>
-        <div class="h-[100px] w-[680px] bg-white rounded-b">
-          <div class="flex justify-center p-[35px]">
-            <button class="">
+        <div className="h-[100px] w-[680px] bg-white rounded-b">
+          <div className="flex justify-center p-[35px]">
+            <button className="">
             </button>
             <div className="flex justify-center pb-[35px]">
-              <Link to="/register" className=" hover:underline">
-                Don't have an account?
+              <Link to="/register" className="hover:underline">
+                Don't have an Account?
               </Link>
           </div>
           
